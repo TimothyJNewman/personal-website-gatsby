e68fb8c29887f334b3af12e432e8aee82a402b9c
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2025-12-21 15:08:56 +0000

    Added rocket paper, updated CV and changed CV embed to object.

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -15,12 +15,17 @@ const CVPage = () => (
       <div className="lg:w-[54rem] px-2 lg:px-0 ">
         <CoverImage title="Curriculum Vitae" />
         <br />
-        <embed
+        {/* <embed
           src="/CV.pdf#view=FitH"
           type="application/pdf"
           className="h-screen w-full rounded"
-        />
-        <p>
+        /> */}
+
+        <object data="/CV.pdf#view=FitH" type="application/pdf" className="h-screen w-full rounded">
+            <p>If the CV is not displayed in the viewer, please open the file with <a href="/CV.pdf#view=FitH">this link</a>.</p>
+        </object>
+
+        <p className='markdown-text'>
             At the moment, I am not looking for work but I am happy to connect with people interested in analog and digital circuit design, data science and digital signal processing. Thanks!
             </p>
       </div>

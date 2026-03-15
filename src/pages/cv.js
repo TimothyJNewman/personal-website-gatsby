d9ef029a91a296a7980304f8864dea475bef7be2
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2026-03-15 19:58:16 +0000

    Fixed CV viewing issues

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -15,24 +15,31 @@ const CVPage = () => (
       <div className="lg:w-[54rem] px-2 lg:px-0 ">
         <CoverImage title="Curriculum Vitae" />
         <br />
-        {/* <embed
-          src="/CV.pdf#view=FitH"
+        <object
+          data="/CV.pdf#view=FitH"
           type="application/pdf"
           className="h-screen w-full rounded"
-        /> */}
-
-        <object data="/CV.pdf#view=FitH" type="application/pdf" className="h-screen w-full rounded markdown-text">
-            <p>If the CV is not displayed in the viewer, please open the file with <a href="/CV.pdf#view=FitH">this link</a>.</p>
+        >
+          <iframe
+            src="/CV.pdf#view=FitH"
+            title="Curriculum Vitae"
+            className="h-screen w-full rounded"
+          />
         </object>
-
-        <p className='markdown-text'>
-            At the moment, I am not looking for work but I am happy to connect with people interested in analog and digital circuit design, data science and digital signal processing. Thanks!
-            </p>
+        <p className="markdown-text mt-2">
+          PDF not displaying?
+          {' '}
+          <a href="/CV.pdf">Download CV</a>
+          .
+        </p>
+        <p className="markdown-text">
+          At the moment, I am not looking for work but I am happy to connect with people
+          interested in analog and digital circuit design, data science and digital signal
+          processing. Thanks!
+        </p>
       </div>
     </LayoutSingleColumn>
   </Layout>
 );
 
 export default CVPage;
-
-// ReactDOM.render(<CVDocument />, document.getElementById('main'));
